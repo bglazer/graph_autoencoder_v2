5f6e6ee846434ce5554163468cf7ce22459d2935
Bryan Glazer <bryan.glazer@vanderbilt.edu>
2022-12-02 00:56:29 -0600

    Adding overlap penalty to loss

--- graph_autoencoder.py
+++ graph_autoencoder.py
@@ -1,7 +1,3 @@
-import torch
-import torch_geometric
-
-import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -108,17 +104,7 @@ class Up(nn.Module):
 
     def forward(self, x1):
         x1 = self.up(x1)
-        # input is CHW
-        # diffY = x2.size()[2] - x1.size()[2]
-        # diffX = x2.size()[3] - x1.size()[3]
 
-        # x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
-        #                 diffY // 2, diffY - diffY // 2])
-        # if you have padding issues, see
-        # https://github.com/HaiyongJiang/U-Net-Pytorch-Unstructured-Buggy/commit/0e854509c2cea854e247a9c615f175f76fbb2e3a
-        # https://github.com/xiaopeng-liao/Pytorch-UNet/commit/8ebac70e633bac59fc22bb5195e513d5832fb3bd
-        # x = torch.cat([x2, x1], dim=1)
-        # x = torch.cat(x1, dim=1)
         return self.conv(x1)
 
 
@@ -127,7 +113,7 @@ class OutConv(nn.Module):
         super(OutConv, self).__init__()
         self.conv = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, kernel_size=1),
-            nn.Sigmoid()
+            nn.LeakyReLU()
             )
 
     def forward(self, x):
