68bd295f91788cd3b1b657462fabeb74c29d86d3
Bryan Glazer <bryan.glazer@vanderbilt.edu>
2022-12-05 14:21:17 -0600

    Mostly working for a single image

--- graph_autoencoder.py
+++ graph_autoencoder.py
@@ -115,6 +115,7 @@ class OutConv(nn.Module):
         super(OutConv, self).__init__()
         self.conv = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, kernel_size=1),
+            nn.LeakyReLU()
             # nn.Sigmoid()
             )
 
